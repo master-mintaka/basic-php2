6bcda7f87da682415b686207c45d96782adb3276
Master Mintaka - John W Granados A <jwga76@gmail.com>
2025-03-11 22:58:30 -0500

    E23 - Implement authorization checks for note access and add response handling for errors

--- controllers/note.php
+++ controllers/note.php
@@ -4,7 +4,19 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'Note';
+$currentUserId = 1;
 
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id']
+])->fetch();
+
+
+if (!$note){
+    abort(404);
+}
+
+if ($note['user_id'] !== $currentUserId){
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
